2ea96e548e6bd9f6f36034efdd3b2f6e197e4a95
optigrit <132610774+optigrit@users.noreply.github.com>
2023-07-16 23:21:34 +0530

    Add files via upload

--- src/Redux/Task/Task-Reducer.jsx
+++ src/Redux/Task/Task-Reducer.jsx
@@ -1,40 +1,141 @@
+import GetValidatedTokenData from "../../utils/helper";
 import {
   CREATE_TASK,
+  CREATE_SUBTASK,
   VIEW_TASK,
   SET_SELECTED_SUBTASK,
   SET_SELECTED_SECTIONS,
   ADD_ASSIGNEE,
   DELETE_ASSIGNEE,
+  SET_TASKS,
+  SET_SUBTASKS,
+  DELETE_TASK,
+  DELETE_SUBTASK,
+  EDIT_TASK,
+  EDIT_SUBTASK,
+  SET_CURRENT_TASK,
+  SET_TASK_PROGRESS,
+  SET_ASSIGNEE_ID,
+  FILE_POST_DATA,
+  CHANGE_POST_DATA,
+  FETCH_TASK_PROGRESS
 } from "./Task-Constants";
-import { v4 as uuid } from "uuid";
+
+const tokenData = GetValidatedTokenData();
 
 const initialState = {
   tasks: [],
-  currentTask: null,
-  subtasks: { course: {}, contest: {}, text: "" },
+  currentTask: [],
+  subtasks: [],
   selectedSubtask: null,
   selectedSections: [],
   taskAssignees: [],
+  role: tokenData?.role,
+  taskProgress: null,
+  assigneeId: null,
+  subtaskPostData: [],
+  fetchTaskProgress: false
 };
 
 const TaskReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SET_TASKS:
+      return {
+        ...state,
+        tasks: action.payload,
+      };
+
+    case SET_SUBTASKS:
+      return {
+        ...state,
+        subtasks: action.payload,
+      };
+
     case CREATE_TASK:
       return {
         ...state,
         tasks: [...state.tasks, action.payload],
       };
+
+    case CREATE_SUBTASK:
+      return {
+        ...state,
+        subtasks: [...state.subtasks, action.payload],
+      };
+
     case VIEW_TASK:
       return {
         ...state,
         currentTask: { ...action.payload },
       };
 
+    case DELETE_TASK:
+      return {
+        ...state,
+        tasks: state.tasks.filter((task) => task.id !== action.payload.id),
+      };
+
+    case DELETE_SUBTASK:
+      return {
+        ...state,
+        subtasks: state.subtasks.filter(
+          (subtask) => subtask.id !== action.payload.id
+        ),
+      };
+
+    case EDIT_TASK:
+      return {
+        ...state,
+        tasks: state.tasks.map((task) =>
+          task.id === action.payload.id ? { ...action.payload } : task
+        ),
+      };
+
+    case EDIT_SUBTASK:
+      return {
+        ...state,
+        subtasks: state.subtasks.map((subtask) =>
+          subtask.id === action.payload.id ? { ...action.payload } : subtask
+        ),
+      };
+
     case SET_SELECTED_SUBTASK:
       state.selectedSubtask = action.payload;
+      break;
 
     case SET_SELECTED_SECTIONS:
       state.selectedSections = action.payload;
+      break;
+
+    case SET_CURRENT_TASK:
+      return {
+        ...state,
+        currentTask: action.payload,
+      };
+
+    case SET_TASK_PROGRESS:
+      return {
+        ...state,
+        taskProgress: action.payload,
+      };
+
+    case SET_ASSIGNEE_ID:
+      return {
+        ...state,
+        assigneeId: action.payload,
+      };
+
+    case FILE_POST_DATA:
+      return {
+        ...state,
+        subtaskPostData: [...state.subtaskPostData, action.payload],
+      };
+
+    case CHANGE_POST_DATA:
+      return {
+        ...state,
+        subtaskPostData: state.subtaskPostData.map((item) => item.subtaskId === action.payload.subtaskId ? {...action.payload} : item),
+      };
 
     case ADD_ASSIGNEE:
       return {
@@ -49,6 +150,12 @@ const TaskReducer = (state = initialState, action) => {
           (assignee) => assignee.id !== action.payload
         ),
       };
+    
+      case FETCH_TASK_PROGRESS:
+        return {
+          ...state,
+          fetchTaskProgress: action.payload
+        }
 
     default:
       return state;
--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,51 +1,12 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import jwt_decode from "jwt-decode";
 import PageNotFound from "../pages/PageNotFound/PageNotFound";
-import { courseAPI } from "../api/requests/courses/courseAPI";
-import { Box, CircularProgress } from "@mui/material";
+import GetValidatedTokenData from "../utils/helper";
 
-const PrivateRoute = ({ children }) => {
-  const Token = localStorage.getItem("Token");
-  const decoded = jwt_decode(Token);
-  const [isValidCourseId, setIsValidCourseId] = useState(true);
-  const [loading, setLoading] = useState(true);
-  const { id } = useParams();
-
-  const checkIdValidity = async () => {
-    try {
-      const response = await courseAPI.getSpecificCourse(id);
-      setIsValidCourseId(response && true);
-    } catch (error) {
-      {
-        error.request.status === 404 && setIsValidCourseId(false);
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    checkIdValidity(id);
-  }, []);
-
-  if (loading)
-    return (
-      <Box
-        sx={{
-          display: "flex",
-          height: "100vh",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <CircularProgress />
-      </Box>
-    );
+const PrivateRoute = ({ children, accessibleTo }) => {
+  const decoded = GetValidatedTokenData();
 
   return (
     <>
-      {decoded.role === "SUPERADMIN" && isValidCourseId ? (
+      {accessibleTo.includes(decoded.role)  ? (
         <>{children}</>
       ) : (
         <PageNotFound />
